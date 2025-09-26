f7a6e7f83c566b19eff5463a9e93c5f1c292ff12
njdpadlan <169112404+njdpadlan@users.noreply.github.com>
2025-09-26 11:47:56 -0600

    revert back server.js

--- strapi-server/config/server.js
+++ strapi-server/config/server.js
@@ -1,19 +1,10 @@
-// module.exports = ({ env }) => ({
-//   host: env('HOST', '0.0.0.0'),
-//   port: env.int('PORT', 1337),
-//   app: {
-//     keys: env.array('APP_KEYS'),
-//   },
-//   webhooks: {
-//     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-//   },
-// });
-
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'https://strapi-server-9p29.onrender.com'), // 👈 important
   app: {
     keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });

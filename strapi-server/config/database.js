ba8f203f631a9fe8541e00787a6c82d983dccf50
njdpadlan <169112404+njdpadlan@users.noreply.github.com>
2025-09-28 18:58:01 -0600

    changed database.js

--- strapi-server/config/database.js
+++ strapi-server/config/database.js
@@ -3,16 +3,14 @@
       connection: {
         client: 'postgres',
         connection: {
-          host: env('DATABASE_HOST', 'localhost'),
+          host: env('DATABASE_HOST', 'dpg-d3b2rgd6ubrc739dea60-a.oregon-postgres.render.com'),
           port: env.int('DATABASE_PORT', 5432),
-          database: env('DATABASE_NAME', 'bank'),
-          user: env('DATABASE_USERNAME', 'postgres'),
-          password: env('DATABASE_PASSWORD', '0000'),
-          schema: env('DATABASE_SCHEMA', 'public'), // Not required
+          database: env('DATABASE_NAME', 'strapi_server_db_nyom'),
+          user: env('DATABASE_USERNAME', 'admin'),
+          password: env('DATABASE_PASSWORD', 'C3n50kZjVvSeAUDiiXnNB4JocMCZq64E'),
           ssl: {
-            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
-          },
-        },
-        debug: false,
+            rejectUnauthorized: false,
+          }
+        }
       },
     });

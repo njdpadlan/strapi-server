6bb10095d71097c60971d049b067f11272b4f444
njdpadlan <169112404+njdpadlan@users.noreply.github.com>
2025-09-28 20:39:13 -0600

    asdkljad

--- strapi-server/config/database.js
+++ strapi-server/config/database.js
@@ -1,29 +1,18 @@
-    // // strapi-api/config/database.js
-    // module.exports = ({ env }) => ({
-    //   connection: {
-    //     client: 'postgres',
-    //     connection: {
-    //       host: env('DATABASE_HOST'),
-    //       port: env.int('DATABASE_PORT'),
-    //       database: env('DATABASE_NAME'),
-    //       user: env('DATABASE_USERNAME'),
-    //       password: env('DATABASE_PASSWORD'),
-    //       ssl: {
-    //         rejectUnauthorized: false,
-    //       }
-    //     }
-    //   },
-    // });
-
-  export default ({ env }) => ({
-  client: "postgres",
-  connection: {
-    host: env("DATABASE_HOST"),
-    port: env.int("DATABASE_PORT", 5432),
-    database: env("DATABASE_NAME"),
-    user: env("DATABASE_USERNAME"),
-    password: env("DATABASE_PASSWORD"),
-    ssl: { rejectUnauthorized: false },
-  },
-  useNullAsDefault: true,
-});
+    // strapi-api/config/database.js
+    module.exports = ({ env }) => ({
+      connection: {
+        client: 'postgres',
+        connection: {
+          host: env('DATABASE_HOST'),
+          port: env.int('DATABASE_PORT'),
+          database: env('DATABASE_NAME'),
+          user: env('DATABASE_USERNAME'),
+          password: env('DATABASE_PASSWORD'),
+          ssl: {
+            rejectUnauthorized: false,
+          }
+        },
+          useNullAsDefault: true,
+      },
+    });
+    
